b67a0ee855a5adf3a6f3aba64dbf1e6a67661e79
Amir Safari <amir2007.safari@gmail.com>
2026-01-08 15:37:53 +0330

    map.c

--- lib/map.c
+++ lib/map.c
@@ -1,41 +1,56 @@
 #include "map.h"
+#include "player.h"
 
 int WORLD_MAP[ROWS][COLUMNS] = {
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-    {0, 0, 0, 1, 0, 0, 0, 0, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
-    {1, 0, 1, 0, 0, 0, 0, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-void DrawMap(int *isPlayerSet)
+void DrawMap(int mode)
 {
     int map_x = player.pos.x / TILE_SIZE;
     int map_y = player.pos.y / TILE_SIZE;
+
+    float scale = mode == STATE_2D ? 1 : MINIMAP_SCALE;
 
     for (int i = 0; i < ROWS; i++)
     {
         for (int j = 0; j < COLUMNS; j++)
         {
-            if (WORLD_MAP[i][j] == 0 && !*isPlayerSet)
-            {
-                player.pos.x = j * TILE_SIZE + PLAYER_RADIUS;
-                player.pos.y = i * TILE_SIZE + PLAYER_RADIUS;
-                *isPlayerSet = 1;
-            }
+            Color cellColor;
 
             if (WORLD_MAP[i][j] == 1)
-                DrawRectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, BLACK);
+                cellColor = BLACK;
             else if (i == map_y && j == map_x)
-                DrawRectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, LIGHTGRAY);
+                cellColor = LIGHTGRAY;
             else if (WORLD_MAP[i][j] == 0)
-                DrawRectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, WHITE);
+                cellColor = WHITE;
 
-            DrawRectangleLines(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
+            DrawRectangle(j * TILE_SIZE * scale + (SCREEN_WIDTH - COLUMNS * TILE_SIZE * scale), i * TILE_SIZE * scale, TILE_SIZE * scale, TILE_SIZE * scale, cellColor);
+            DrawRectangleLines(j * TILE_SIZE * scale + (SCREEN_WIDTH - COLUMNS * TILE_SIZE * scale), i * TILE_SIZE * scale, TILE_SIZE * scale, TILE_SIZE * scale, GRAY);
         }
     }
-}    
+}
+
+void InitEditMap()
+{
+    if (!((int)(GetMouseX() / TILE_SIZE) == (int)(player.pos.x / TILE_SIZE) && (int)(GetMouseY() / TILE_SIZE) == (int)(player.pos.y / TILE_SIZE)))
+    {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            WORLD_MAP[(int)(GetMouseY() / TILE_SIZE)][(int)(GetMouseX() / TILE_SIZE)] = 1;
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            WORLD_MAP[(int)(GetMouseY() / TILE_SIZE)][(int)(GetMouseX() / TILE_SIZE)] = 0;
+    }
+}
