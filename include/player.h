a3b18297bf641ecdcc9e391a9e5fbaa3ada589b5
Amir Safari <amir2007.safari@gmail.com>
2026-01-08 15:37:30 +0330

    player.h

--- include/player.h
+++ include/player.h
@@ -2,25 +2,27 @@
 #define PLAYER_H
 
 #include <math.h>
-#include "raylib.h"
 #include "map.h"
+#include "raylib.h"
+
+#define PLAYER_RADIUS 10
+#define FOV_SCALE 0.66f
 
 #define BASE_SPEED 200
-#define BASE_ROT_SPEED 2.5f
-#define PLAYER_RADIUS 10
+#define BASE_ROT_SPEED 1.75f
 
-typedef struct
-{
-    Vector2 pos;
-    Vector2 dir;
+typedef struct {
+   Vector2 pos;
+   Vector2 dir;
+   Vector2 plane;
 } Player;
 
 extern Player player;
 
-void DrawPlayer();
-
+void PlacePlayer();
+void DrawPlayer(int mode);
 void InitMobility(float moveSpeed);
-
 void InitRotation(float rotSpeed);
+void CastRays(int x);
 
 #endif
